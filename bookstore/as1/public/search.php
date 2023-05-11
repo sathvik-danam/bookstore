ff46e80bc4b97a9cd703d1610d9933ee97e74e92
Sathvik Danam <101180463+sathvik-danam@users.noreply.github.com>
2023-05-11 18:08:37 +0100

    Delete bookstore/as1 directory

--- bookstore/as1/public/search.php
+++
@@ -1,9 +0,0 @@
-<?php
-include("theconnection.php");
-?>
-	
-
-<form method="post" action="AuctionPage.php">
-			<input type="text" name="search" placeholder="Search a product in auction (title or description)" />
-			<input type="submit" name="submit" value="Search" />
-		</form>
